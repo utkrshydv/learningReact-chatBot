989872c113506b0603bba31381110ac1a96aeee0
utkrshydv <utkarsh4socials@gmail.com>
2025-08-09 23:08:39 +0530

    complete core features

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,28 @@
+import RobotImage from '../assets/robot.png'
+import UserImage from '../assets/user.png'
+import './ChatMessage.css'
+
+
+export function ChatMessage({message, sender}){
+  return(
+    <div className={
+      sender === 'user'
+      ? 'chat-message-user'
+      : 'chat-message-robot'
+    }>
+      {sender === 'robot' && (
+        <img
+        src={RobotImage} 
+        className='chat-message-profile'
+        />
+      )}
+      <div className="chat-message-text">
+              {message}
+      </div>
+      {sender === 'user' && (
+        <img 
+        src={UserImage} className='chat-message-profile' />
+      )}
+    </div>
+  );
+}
---
+++ src/components/ChatMessages.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react";
+import { ChatMessage } from "./ChatMessage";
+import './ChatMessages.css'
+
+export function ChatMessages({chatMessages}){
+
+  const chatMessageRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = chatMessageRef.current;
+    if(containerElem){
+    containerElem.scrollTop = containerElem.scrollHeight;
+  }
+  }, [chatMessages])
+  return (
+    <div className="chat-messages-container" ref={chatMessageRef}>
+      {chatMessages.map((chatMessage) => {
+        return(
+        <ChatMessage 
+        message = {chatMessage.message}
+        sender = {chatMessage.sender}
+        key={chatMessage.id}
+        />
+      )
+    })}
+    </div>
+)
+}
+
+export default ChatMessages;
